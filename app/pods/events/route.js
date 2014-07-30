3b9b830de2d054a8ac02d32244949cfe63cef658
trabus <trabus@gmail.com>
2014-07-30 08:09:05 -0700

    added events and forums pods

---
+++ app/pods/events/route.js
@@ -0,0 +1,5 @@
+// eventsRoute
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+});
---
+++ app/pods/forums/route.js
@@ -0,0 +1,5 @@
+// forumsRoute
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+});
